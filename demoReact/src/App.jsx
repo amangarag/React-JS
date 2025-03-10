7e940d6c0d345f4281b8819e1269bab0874786ae
amangarag <amangarag1234@gmail.com>
2025-03-10 12:49:14 +0530

    Demo File Added

---
+++ demoReact/src/App.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+
+function App(params) {
+  return (
+    <div>
+      <h1>My React App</h1>
+      <p>Open the console to check my demo component</p>
+    </div>
+  )
+}
+
+export default App;
